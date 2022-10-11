0bba322d7639467afd9f3073716f044978e922c6
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-10-11 19:36:47 -0400

    update

---
+++ my-app/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">📸</span>Oh Snap!
+        </a>
+      </h2>
+    </header>
+  );
+}
+
+export default Nav;
